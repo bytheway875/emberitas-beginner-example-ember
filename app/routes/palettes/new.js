b6d6fad8a987c1cb04cb339893de3ff4c00c067b
Shannon Byrne <s.byrne2254@gmail.com>
2017-01-15 16:48:56 -0600

    Add default data for our new model and create our first two way binding

--- app/routes/palettes/new.js
+++ app/routes/palettes/new.js
@@ -1,4 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    return {
+      id: null,
+      name: 'My New Palette',
+      colors: [
+        {red: randomColorCode(), blue: randomColorCode(), green: randomColorCode()},
+        {red: randomColorCode(), blue: randomColorCode(), green: randomColorCode()},
+        {red: randomColorCode(), blue: randomColorCode(), green: randomColorCode()},
+        {red: randomColorCode(), blue: randomColorCode(), green: randomColorCode()},
+        {red: randomColorCode(), blue: randomColorCode(), green: randomColorCode()}
+      ]
+    };
+  },
 });
+
+function randomColorCode(){
+  return Math.floor(Math.random() * 256)
+}
